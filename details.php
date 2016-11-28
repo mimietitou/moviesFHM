a63227dc108727665fd46360d6c08c9068c4a85a
hermelen <hermelen.peris@gmail.com>
2016-11-28 16:58:13 +0100

    Her 20181128 17:00

--- details.php
+++ details.php
@@ -1,35 +1,61 @@
 <?php include 'include/pdo.php' ?>
 <?php include 'include/functions.php'; ?>
-
+<?php include 'include/header.php'; ?>
 <?php if(!empty($_GET['slug'])){
 
   $slug = $_GET['slug'];
   $sql = "SELECT *  FROM movies_full WHERE slug = '$slug'";
   $query = $pdo->prepare($sql);
   $query->execute();
-  $posters = $query->fetchAll();
+  $posters = $query->fetch();
 
-  if(!empty())
+
+
+    echo '<div class="films"><img src="posters/'. $posters['id'] .'.jpg" alt="'. $posters['title'] .'"/></div>';
+    foreach ($posters as $poster => $value ) {
+      echo '<p class="description">'.$poster.' : '.$value.'</p>';
+    };
+
+    $id_movie = $posters['id'];
+    $id_user = $_SESSION['user']['id'];
+
+
+    if(!empty($_POST['submit'])){
+
+      $sql = "INSERT INTO movies_user_note (id_movie, id_user, created_at, status) VALUES (:id_movie, :id_user, NOW(), 'a_voir')";
+      $query = $pdo->prepare($sql);
+      $query->bindvalue(':id_movie', $id_movie,PDO::PARAM_INT);
+      $query->bindvalue(':id_user',$id_user,PDO::PARAM_INT);
+      $query->execute();
+
+    }
+
+    echo $current_film = $posters['id'];
+
+    $sql = "SELECT * FROM movies_user_note WHERE status = 'a_voir' && id_user = :id_user";
+    $query = $pdo->prepare($sql);
+    $query->bindvalue(':id_user',$id_user,PDO::PARAM_INT);
+    $query->execute();
+    $selecteds = $query->fetchAll();
+
+     foreach($selecteds as $selected){
+       if ($posters['id'] = $selected['id_movie']){
+        echo $selected['id_movie'];
+       }
+     }
+
 
 
   ?>
 
-<?php include 'include/header.php';
-
-
-  foreach ($posters as $poster ) {
-    echo '<div class="films"><img src="posters/'. $poster['id'] .'.jpg" alt="'. $poster['title'] .'"/></div>';
-    foreach ($poster as $details => $value ) {
-     echo '<p class="description">'.$details.' : '.$value.'</p>';
-
-   } ?>
-  <form class="" action="index.html" method="post">
-
+  <form action="" method="post">
+    <input type="submit" name="submit" value="Film à voir absolument!">
   </form>
-  <input type="submit" name="film_a_voir" value="Film à voir absolument!" method="post">
+
 
   <?php
-  }
+
+
 
 }?>
 
